949e3c9d20b7b1f798bd6376e5c5cf9b84ee14c2
Anthony Brown <abrown4123@gmail.com>
2017-03-27 22:00:13 -0500

    Almost done with the list maker. Still needs some styling.

---
+++ List Maker/todo.js
@@ -0,0 +1,26 @@
+//check off todos with click
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+//delete todo
+$("ul").on("click", "span", function(e){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	e.stopPropagation();
+});
+
+
+//add new todo
+$('input').keypress(function(e){
+	if(e.which === 13){
+		//new todo text after hitting enter
+		var text = $(this).val();
+		$(this).val("");
+		//create new li and add to ul
+		$("ul").append("<li><span>X</span> " + text + "</li>");
+		
+		
+	}
+});
